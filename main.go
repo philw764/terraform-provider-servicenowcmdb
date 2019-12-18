49bf1df1f2435b9eb040c9f06d86c2d1d5910326
philw764 <philw764@bigpond.com>
2019-12-18 13:03:12 +1000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"terraform-provider-servicenowcmdb/servicenowcmdb/resources"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() terraform.ResourceProvider {
+			return resources.Provider()
+		},
+	})
+}
